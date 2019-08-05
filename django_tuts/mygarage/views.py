5be8b588862b571a536b4325a1b8214adce475b6
manojm <m.more@easternenterprise.com>
2019-08-05 15:02:24 +0530

    adding templates

--- django_tuts/mygarage/views.py
+++ django_tuts/mygarage/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 
 def Home(request):
-    return HttpResponse('<h1>This is test django</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
 
 def About(request):
-    return HttpResponse('<h1>This is About django</h1>')
+    return render(request, 'about.html')
